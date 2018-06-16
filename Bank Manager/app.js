7b421ac96612695d01f3ff6409b0f08999d0323d
SaajanM <saajan.maslanka@gmail.com>
2018-06-16 16:42:45 -0700

    Test 03

--- Bank Manager/app.js
+++ Bank Manager/app.js
@@ -11,20 +11,37 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
+    var args;
+    var cmd;
     if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-
+        args = message.substring(1).split(' ');
+        cmd = args[0];
         args = args.splice(1);
         switch (cmd) {
             // !ping
-            case 'ping':
+            case 'test':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Pong!'
+                    message: 'All online!'
                 });
                 break;
             // Just add any case commands if you want to..
+        }
+    }
+    if (message.substring(0, 1) == "$") {
+        args = message.substring(1).split(" ");
+        cmd = args[0];
+        args = args.splice(1);
+        switch (cmd) {
+            case "give":
+                if (parseInt(args[0]) >= 0 && args[1] !== "" && parseInt(args[0]) == args[0] && args[1].includes("<@") {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: "Deposited $" + args[0] + "!";
+                    });
+                }
+                break;
+        
         }
     }
 });
