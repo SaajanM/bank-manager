ca272b77420c06d913b5b1af472daa0909f210df
SaajanM <saajan.maslanka@gmail.com>
2018-06-18 13:21:40 -0700

    Give cmd v1.0

--- Bank Manager/commands-2/give.js
+++ Bank Manager/commands-2/give.js
@@ -2,6 +2,7 @@ exports.run = (client, message, args) => {
     if (!args || args.length < 2) return;
     if (args[0].charAt(0) == "<" || args[0] <= 0) return;
     if (args[1].charAt(0) != "<") return;
-    client.channels.get('456549631900516354').send("<@" + message.author.id + "> Is making a deposit of $" + args[0]);
+    if ("<@" + message.author.id + ">" == args[1]) return;
+    client.channels.get('456549631900516354').send("<@" + message.author.id + "> wants to make a deposit of $" + args[0]);
 
 };
