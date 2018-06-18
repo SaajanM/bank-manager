a623f697aef9394ff7e53002f2672767b266ea28
SaajanM <saajan.maslanka@gmail.com>
2018-06-18 13:12:21 -0700

    Fix give cmd

--- Bank Manager/commands-2/give.js
+++ Bank Manager/commands-2/give.js
@@ -2,6 +2,6 @@ exports.run = (client, message, args) => {
     if (!args || args.length < 2) return;
     if (args[0].charAt(0) == "<" || args[0] <= 0) return;
     if (args[1].charAt(0) != "<") return;
-    client.channels.get(456549631900516354).send("<@" + message.author.id + "> Is making a deposit of $" + args[0]);
+    client.channels.get('456549631900516354').send("<@" + message.author.id + "> Is making a deposit of $" + args[0]);
 
 };
