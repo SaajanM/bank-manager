403f92f1cb3142ad28f2d2485894a1a8f07442a3
SaajanM <saajan.maslanka@gmail.com>
2018-06-18 13:04:58 -0700

    Give command + fixes

--- Bank Manager/commands-1/reload.js
+++ Bank Manager/commands-1/reload.js
@@ -1,21 +1,23 @@
 exports.run = (client, message, args) => {
-    if (!args || args.size < 1) return message.reply("`Missing Required Argument: commandset`");
-    if (!args || args.size < 2) return message.reply("`Missing Required Argument: command");
+    var userId = message.author.id;
+    if (userId != client.config.ownerID) return message.channel.send("Sorry <@" + userId + ">\nThis command is meant for the bot owner.")
+    if (!args || args.length < 1) return message.channel.send("`Missing Required Argument: commandset`");
+    if (!args || args.length < 2) return message.channel.send("`Missing Required Argument: command");
     const commandSet = args[0];
     const commandName = args[1];
     // Check if the command exists and is valid
     if (commandSet == 1) {
         if (!client.commands1.has(commandName)) {
-            return message.reply("That command does not exist");
+            return message.channel.send("That command does not exist");
         }
         const path = "./";
     } else if (commandSet == 2) {
         if (!client.commands2.has(commandName)) {
-            return message.reply("That command does not exist");
+            return message.channel.send("That command does not exist");
         }
         const path = "../commands-2/";
     } else {
-        return message.reply("Invalid Command Set choose between:\n1 (main)\n2 (secondary)");
+        return message.channel.send("Invalid Command Set choose between:\n1 (main)\n2 (secondary)");
     }
 
     // the path is relative to the *current folder*, so just ./filename.js
@@ -30,5 +32,5 @@ exports.run = (client, message, args) => {
         const props = require(`${path}${commandName}.js`);
         client.commands.set(commandName, props);
     }
-    message.reply(`The command ${commandName} has been reloaded`);
+    message.channel.send(`The command ${commandName} has been reloaded`);
 };
--- Bank Manager/commands-2/give.js
+++ Bank Manager/commands-2/give.js
@@ -1,14 +1,7 @@
 exports.run = (client, message, args) => {
-    message.reply("WIP");
-
-
-
-
-
-
-
-
-
-
+    if (!args || args.length < 2) return;
+    if (args[0].charAt(0) == "<" || args[0] <= 0) return;
+    if (args[1].charAt(0) != "<") return;
+    client.channels.get(456549631900516354).send("<@" + message.author.id + "> Is making a deposit of $" + args[0]);
 
 };
