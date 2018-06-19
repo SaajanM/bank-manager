0529c1a820d0242ceff10c77dc6bac492e54baae
SaajanM <saajan.maslanka@gmail.com>
2018-06-18 18:15:17 -0700

    Give command v3.0

--- Bank Manager/commands-2/give.js
+++ Bank Manager/commands-2/give.js
@@ -1,12 +1,17 @@
 exports.run = (client, message, args) => {
+    var isAVault = false;
+    for (var v in client.config.vaults) {
+        if (client.config.vaults[v] == message.channel.id) {
+            isAVault = true;
+        }
+    }
+    if (!isAVault) return;
     if (!args || args.length < 2) return;
     if (args[0].charAt(0) == "<" || args[0] <= 0) return;
     if (args[1].charAt(0) != "<") return;
     if ("<@" + message.author.id + ">" == args[1]) return;
     const ownersRole = message.guild.roles.get("455439873810104340");
-    console.log(ownersRole.name);
     const owners = ownersRole.members;
-    //console.log(owners);
     var isOneOnline = false;
     owners.forEach(function (presence) {
         if (presence.status == "online") {
@@ -15,6 +20,4 @@ exports.run = (client, message, args) => {
     })
     if (isOneOnline) return;
     message.channel.send("Hey <@&455439873810104340>!\n<@" + message.author.id + "> wants to make a deposit of $" + args[0]);
-
-
 };
