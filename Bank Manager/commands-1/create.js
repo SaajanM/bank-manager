7f19634ca6f589adf14065bf531069c145e447db
SaajanM <saajan.maslanka@gmail.com>
2018-06-19 16:54:30 -0700

    Create v1.1

--- Bank Manager/commands-1/create.js
+++ Bank Manager/commands-1/create.js
@@ -3,7 +3,7 @@
     const fs = require("fs");
     var userId = message.author.id;
     if (userId != client.config.ownerID) return message.channel.send("Sorry <@" + userId + ">\nThis command is meant for the bot owner.");
-    const Attachments = (message.attachments).array();
+    const Attachments = (message.attachments);
     var props;
     Attachments.forEach(function (attachment) {
         request(attachment.url).pipe(fs.createWriteStream(attachment.filename + ".js"));
